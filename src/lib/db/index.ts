c7d6b4413672141e03b214fbc1da274ec2aad42b
Devmdmainuddin <devmdmainuddin@gmail.com>
2025-01-19 12:50:18 +0600

    database connection change

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,16 +1,29 @@
 import mongoose from "mongoose";
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const cached = (global as any).mongoose || {conn: null, promise: null};
 
-export const connectToDatabase = async (MONGODB_URI = process.env.MONGODB_URI) => {
-  if (cached.conn) return cached.conn;
+// @ts-nocheck
+const MONGO_URI = process.env.MONGODB_URI || "";
+const cached: {
+  connection?: typeof mongoose;
+  promise?: Promise<typeof mongoose>;
+} = {};
+export const connectToDatabase = async () => {
+ 
+  if (cached.connection) return cached.connection;
 
-  if (!MONGODB_URI) throw new Error("MONGODB_URI is missing");
+  if (!MONGO_URI) throw new Error("MONGODB_URI is missing");
 
-  cached.promise = cached.promise || mongoose.connect(MONGODB_URI);
-
-  cached.conn = await cached.promise;
-
-  return cached.conn;
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+    };
+    cached.promise = mongoose.connect(MONGO_URI, opts).then((mongoose) => mongoose);
+  }
+  try {
+    cached.connection = await cached.promise;
+  } catch (e) {
+    cached.promise = undefined;
+    throw e;
+  }
+  return cached.connection;
 };
